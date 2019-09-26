8835d5c02a3b3d9c11b4ca857d3915ebff5306b9
roncarr880 <roncarr880@gmail.com>
2019-09-26 09:20:56 -0400

    Add files via upload

--- CNC_3018_SGG.pl
+++ CNC_3018_SGG.pl
@@ -3,7 +3,8 @@
  use warnings;
  use Win32::GUI();
 
- #output file sgg_out.nc
+ my $filename = 'sgg_out.nc';
+ open( my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 
 
  my $big_font = Win32::GUI::Font->new(        # X Y Z position font
@@ -83,9 +84,9 @@
             -height => 30,
             -width => 100
        );
-       $main->{Spindle}->Min(0);
-       $main->{Spindle}->Max(1000);
-       $main->{Spindle}->Pos(800);
+       $main->{Spindle}->Min(800);
+       $main->{Spindle}->Max(10000);
+       $main->{Spindle}->Pos(5000);
        
        $main->AddLabel(
            -text => "Feed Rate",
@@ -101,8 +102,8 @@
             -width => 100,
        );
        $main->{Feed}->Min(10);
-       $main->{Feed}->Max(200);
-       $main->{Feed}->Pos(75);
+       $main->{Feed}->Max(400);
+       $main->{Feed}->Pos(200);
 
        $main->AddLabel(
            -text => "Depths:",
@@ -204,7 +205,7 @@
 
        my $but3 = $main->AddButton(
            -name => "Header",
-           -text => "Start",
+           -text => "Reset",
            -top  => 20,
            -left => 460,
       );
@@ -224,14 +225,49 @@
            -uppercase => 1
       );
 
-
-
-my $step3 = $main->AddButton(     #debug button
-      -name => "TEN_K",
-      -text => "10 K",
-      -top  => 400,
-      -left => 100,
+      my $txt5 = $main->AddTextfield(
+           -name   => "Radius",
+           -left   => 145,
+           -top    => 300,
+           -width  => 60,
+           -height => 20,
+           -multiline => 0,
+           -font => $medium_font,
+           -prompt => "Arc Radius",
+           -text => "10.0",
+           );
+       $main->AddLabel(
+           -text => "Clockwise ARC to Destination",
+           -top  => 330,
+           -left => 40,
+           -font => $medium_font
+       );
+       my $but5 = $main->AddButton(
+           -name => "CL_ARC",
+           -text => "CL",
+           -top  => 330,
+           -left => 360,
+       );
+       $main->AddLabel(
+           -text => "Counter Clockwise ARC",
+           -top  => 360,
+           -left => 40,
+           -font => $medium_font
+       );
+       my $but6 = $main->AddButton(
+           -name => "CC_ARC",
+           -text => "CC",
+           -top  => 360,
+           -left => 360,
       );
+
+
+# my $step3 = $main->AddButton(     #debug button
+#      -name => "TEN_K",
+#      -text => "10 K",
+#      -top  => 400,
+#      -left => 100,
+#      );
 
 
     $main->Move( 200,100 );
@@ -239,39 +275,150 @@ my $step3 = $main->AddButton(     #debug button
     Win32::GUI::Dialog();
 
     sub Main_Terminate {
+       close $fh;
        return -1;
     }
 
 # debug button
-sub TEN_K_Click {
+#sub TEN_K_Click {
 
- print STDERR $main->{Spindle}->Pos();   print STDERR "  ";
- print STDERR $main->{Feed}->Pos();      print STDERR "  ";
- print STDERR $main->{Drill_Depth}->GetLine(0);
- print STDERR "\n"; 
-  
-}
+# print STDERR $main->{Spindle}->Pos();   print STDERR "  ";
+# print STDERR $main->{Feed}->Pos();      print STDERR "  ";
+# print STDERR $main->{Drill_Depth}->GetLine(0);
+# print STDERR "\n";  
+#}
 
 sub Header_Click {
-   
+   my $x = $txt4->GetLineCount();
    $txt4->Append("( Header Gcode )\r\n");
    $txt4->Append("G21  ( mm )\r\n");
    $txt4->Append("G90  ( absolute )\r\n");
    $txt4->Append("M5   ( stop spindle )\r\n");
    $txt4->Append("G0  Z10.0    ( raise the tool )\r\n" );
    $txt4->Append("G0  X0.0 Y0.0\r\n\r\n");
+   my $y = $txt4->GetLineCount();
 
+   print2file( $x, $y );
+   reset_position( 10 );
+}
+
+sub print2file {
+
+   my ( $s, $e ) = @_;
+   #print STDERR $s . ' ' . $e . "\n";   # ? , always reporting one more than actual
+   $s = $s - 1;
+   $e = $e - 1;
+
+   while ($s < $e ) {
+      print $fh $txt4->GetLine($s) . "\n";
+      $s = $s + 1;
+   }
 }
 
 sub Footer_Click {
 
+   my $x = $txt4->GetLineCount();
    $txt4->Append("\r\n( Footer )\r\n");
-   $txt4->Append("M5\r\nG0   Z10.0\r\nG0 X0.0 Y0.0\r\nM30   ( End )\r\n");
-
+   $txt4->Append("M5\r\nG0   Z10.0\r\nG0  X0.0 Y0.0\r\nM30   ( End )\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y ); 
+   reset_position( ); 
 }
 
 sub Goto_Click {
 
-   $txt4->Append("G0   Z10.0\r\n");    # safe tool height
-   $txt4->Append("G0   X" . $main->{X}->GetLine(0) . "  Y" . $main->{Y}->GetLine(0) . "\r\n");
+   my $x = $txt4->GetLineCount();
+   $txt4->Append("G0  Z10.0   (GoTo)\r\n");    # safe tool height
+   $txt4->Append("G0  X" . $main->{X}->GetLine(0) . "  Y" . $main->{Y}->GetLine(0) . "\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y );
+   update_position(10);
+
 }
+
+sub Drill_Click {
+
+   my $rate = $main->{Feed}->Pos();
+   $rate /= 8;
+   my $x = $txt4->GetLineCount();
+   $txt4->Append("M3  S" . $main->{Spindle}->Pos() . "\r\n");
+   $txt4->Append("G0  Z0.1    ( Drill )\r\n");     # fast to close to work
+   $txt4->Append("G1  Z-" . $main->{Drill_Depth}->GetLine(0) );
+   $txt4->Append("  F" . $rate . "\r\n");
+   $txt4->Append("G1  Z0.1   F" . $rate*8 . "\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y );
+   update_position(0.1);
+
+}
+
+sub Mill_Click {
+
+   my $rate = $main->{Feed}->Pos();
+   my $x = $txt4->GetLineCount();
+   $txt4->Append("M3  S" . $main->{Spindle}->Pos() . "\r\n");
+   $txt4->Append("G0  Z0.1    ( Mill )\r\n");     # fast to close to work
+   $txt4->Append("G1  Z-" . $main->{Mill_Depth}->GetLine(0) );
+   $txt4->Append("  F" . $rate/8 . "\r\n");
+   $txt4->Append("G1  X" . $main->{X}->GetLine(0) . "   Y" . $main->{Y}->GetLine(0));
+   $txt4->Append("   F" . $rate . "\r\n");
+   $txt4->Append("G1  Z0.1   F" . $rate*2 . "\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y );
+   update_position(0.1);
+}
+
+sub CL_ARC_Click {
+
+   my $rate = $main->{Feed}->Pos();
+   my $x = $txt4->GetLineCount();
+   $txt4->Append("M3  S" . $main->{Spindle}->Pos() . "\r\n");
+   $txt4->Append("G0  Z0.1    ( CL Arc )\r\n");     # fast to close to work
+   $txt4->Append("G1  Z-" . $main->{Mill_Depth}->GetLine(0) );
+   $txt4->Append("  F" . $rate/8 . "\r\n");
+   $txt4->Append("G2  X" . $main->{X}->GetLine(0) . "   Y" . $main->{Y}->GetLine(0));
+   $txt4->Append("   F" . $rate . "   R" . $main->{Radius}->GetLine(0) . "\r\n");
+   $txt4->Append("G1  Z0.1   F" . $rate*2 . "\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y );
+   update_position(0.1);
+}
+
+sub CC_ARC_Click {
+
+   my $rate = $main->{Feed}->Pos();
+   my $x = $txt4->GetLineCount();
+   $txt4->Append("M3  S" . $main->{Spindle}->Pos() . "\r\n");
+   $txt4->Append("G0  Z0.1    ( CC Arc )\r\n");     # fast to close to work
+   $txt4->Append("G1  Z-" . $main->{Mill_Depth}->GetLine(0) );
+   $txt4->Append("  F" . $rate/8 . "\r\n");
+   $txt4->Append("G3  X" . $main->{X}->GetLine(0) . "   Y" . $main->{Y}->GetLine(0));
+   $txt4->Append("   F" . $rate . "   R" . $main->{Radius}->GetLine(0) . "\r\n");
+   $txt4->Append("G1  Z0.1   F" . $rate*2 . "\r\n");
+   my $y = $txt4->GetLineCount();
+   print2file( $x, $y );
+   update_position(0.1);
+}
+
+
+sub update_position {
+
+  my $x = $main->{X}->GetLine(0);
+  my $y = $main->{Y}->GetLine(0);
+  my ( $z ) = @_;
+
+  $X_Position->Text( $x );
+  $Y_Position->Text( $y );
+  $Z_Position->Text( $z );
+
+}
+
+sub reset_position {
+
+  $X_Position->Text( '0.00' );
+  $Y_Position->Text( '0.00' );
+  $Z_Position->Text( '10.0' );
+
+
+}
+
